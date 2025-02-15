6b8f78b0051564eac53880f663df0b0a8382db8e
Antonio Maragno <antonio.maragno@edu.unito.it>
2025-02-15 23:51:23 +0100

    Add files via upload

---
+++ pEvent.h
@@ -0,0 +1,32 @@
+#ifndef PEVENT_H
+#define PEVENT_H
+
+#include "TObject.h"
+
+class pEvent : public TObject() {
+
+    public:
+
+
+
+
+
+
+
+
+
+
+
+    private:
+
+
+
+
+
+
+
+    ClassDef(pEvent,1);
+};
+
+
+#endif
---
+++ pHit.h
@@ -0,0 +1,32 @@
+#ifndef PHIT_H
+#define PHIT_H
+
+
+#include "TObject.h"
+
+class pHit : public TObject() {
+
+    public:
+
+
+
+
+
+
+
+
+
+
+
+    private:
+
+
+
+
+
+
+
+    ClassDef(pHit,1);
+};
+
+#endif
