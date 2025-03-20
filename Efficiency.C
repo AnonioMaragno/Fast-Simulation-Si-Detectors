9073ecdadb96eabe0fc4aeea75804cb8b95d6cac
Antonio <antonio.maragno@edu.unito.it>
2025-03-20 19:02:45 +0100

    Efficienze istogrammi

---
+++ Efficiency.C
@@ -0,0 +1,103 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TNtuple.h"
+#include "TBranch.h"
+#include "TMath.h"
+#include "pEvent.h"
+#include "TClonesArray.h"
+#include "Riostream.h"
+#include "TRandom3.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TGraph.h"
+using std::to_string;
+
+const int nHist = 10;
+
+
+void Efficiency() {
+
+    // Lettura file input e ntuple
+    TFile fileIn("treeReconstructed.root");
+    float flag, zRec, zTrue, mult;
+    TNtuple *ntuple = (TNtuple*) fileIn.Get("nt");
+    ntuple->SetBranchAddress("flag", &flag);
+    ntuple->SetBranchAddress("zrec", &zRec);
+    ntuple->SetBranchAddress("ztrue", &zTrue);
+    ntuple->SetBranchAddress("mult", &mult);
+
+    // Creazione file output in cui salvare gli istogrammi
+    TFile fileOut("histograms.root", "RECREATE");
+
+    // Variabili ausiliarie
+    const double zMin = -4*53;
+    const double zMax = 4*53;
+    const double residMin = -10;
+    const double residMax = 10;
+
+    // Creazione istogrammi
+    /*TH1D *vHist[nHist];
+    int multArray[nHist]={3,5,10,15,20,30,40,50,60,70};
+    char name[30];
+    char title[50];
+    for (int i=0; i<nHist; i++){
+        snprintf(name,15,"h%d",smm[i]);
+        snprintf(title,50,"Risoluzione con molteplicità %d",smm[i]);
+        vHist[i] = new TH1D(name, title, 100, residMin, residMax);
+    }*/
+    TH2D *histo = new TH2D("hist", "Histogram", 70, 0., 70., 50, residMin, residMax);
+    double resid;
+
+    for(int ev=0; ev<ntuple->GetEntries(); ev++){
+        
+        ntuple->GetEvent(ev);
+        // qui zrec contiene il valore corrente di zrec; mult il corrente valore di mult, etc
+        cout << ev << ") " << "zTrue = " << zTrue << " zRec = " << zRec << endl;
+
+        // Riempio histo
+        resid = zRec - zTrue;
+        if (flag == 1) {
+            histo->Fill(mult, residMin);
+
+        }
+        else {
+            histo->Fill(mult, residMin-1);
+        }
+    }
+
+    TH1D *vt[14];
+    char name[80];
+    double eff[14];
+    double multArray[14];
+    double num;
+    double den;
+
+    for(int i=0; i<14; i++) {
+        
+        //snprintf(name, "h%i", name[i]);
+        vt[i] = histo->ProjectionX("h", 5*i, 5*(i+1));
+        num = vt[i]->Integral();
+        den = vt[i]->GetEntries();
+        eff[i] = num/den;
+        multArray[i] = 5*i;
+
+    }
+
+    TGraph *EffMult = new TGraph(14,eff,multArray);
+
+
+
+    
+
+    
+
+
+
+
+    fileIn.Close();
+    fileOut.Close();
+
+
+
+
+}
